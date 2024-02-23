928b5dc64c5ca232529bb6ae399f3cf360552efa
Sanjay <sanjaypanihar@gmail.com>
2024-02-23 21:02:12 +0530

    data render to lead index

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -9,17 +9,18 @@ use Illuminate\Support\Facades\Cache;
 class LeadController extends Controller
 {
     public function index() {
-        $cacheKey = 'leads_data';
+        $cacheKey = 'leads_index';
+        $view = Cache::remember($cacheKey, now()->addMinutes(10), function () {
+            $leads = Lead::with(['user' => function ($query) {
+                        $query->where('status', 'ACTIVE');
+                    }])
+                    ->whereIn('type', ['WEB', 'MOBILE', 'AUTO', 'EMBEDDED'])
+                    ->get()
+                    ->groupBy('type');
 
-        $leads = cache()->remember($cacheKey, now()->addMinutes(10), function () {
-            return Lead::with(['user' => function ($query) {
-                    $query->where('status', 'ACTIVE');
-                }])
-                ->whereIn('type', ['WEB', 'MOBILE', 'AUTO', 'EMBEDDED'])
-                ->get()
-                ->groupBy('type');
+            return view('leads.index', compact('leads'))->render();
         });
 
-        return view('leads.index', compact('leads'));
-    }
+        return $view;
+            }
 }
